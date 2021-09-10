fa5106a5772da291cb48e83107d9edbc3d6261b4
AllHailTheSheep <21fasickb@bubblers.us>
2021-09-10 15:20:38 -0400

    remove old calls

--- listener.py
+++ listener.py
@@ -3,12 +3,12 @@ from uttils import getImagesInFolder, openImage
 
 folderToListen = input("Input path to folder to listen in: ")
 # we run this once to get the images that are already in the target folder
-seenImages = getImagesInFolder(folderToListen, recursive=True)
+seenImages = getImagesInFolder(folderToListen)
 
 while True:
     time.sleep(1)
     # get the current list of images in folder
-    currentImages = getImagesInFolder(folderToListen, recursive=True)
+    currentImages = getImagesInFolder(folderToListen)
     for image in currentImages:
         # if we have an image in the current images that appeared since the last check, we open it
         if image not in seenImages:
